e135a20cd554b0e71784cb340a6cffb82775dedd
Mortis66666 <mortisloverbs@gmail.com>
2023-04-14 14:04:08 +0800

    first commit

---
+++ js/sketch.js
@@ -0,0 +1,11 @@
+const canvasWidth = window.innerWidth * 0.7;
+const canvasHeight = window.innerHeight;
+
+function setup() {
+    let canvas = createCanvas(canvasWidth, canvasHeight);
+    canvas.parent("main");
+}
+
+function draw() {
+    background(1);
+}
