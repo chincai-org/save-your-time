82d24de3eed5ad74584f73ac4f740d9c1ae3f495
Mortis66666 <mortisloverbs@gmail.com>
2023-04-19 23:20:48 +0800

    custom debug color

--- js/monster.js
+++ js/monster.js
@@ -4,33 +4,35 @@ class Monster extends Sprite {
         y,
         vector,
         size = 30,
+        debugColor,
         health = monsterHealth,
         speed = monsterSpeed,
         damage = monsterDamage,
         reward = 1
     ) {
         super(x, y, vector, size, health, speed, damage);
+        this.debugColor = debugColor;
         this.reward = reward;
     }
 
     static normal(x, y, vector) {
-        return new Monster(x, y, vector, 30, 5, 0.2, 20, 3);
+        return new Monster(x, y, vector, 30, black, 5, 0.2, 20, 3);
     }
 
     static tank(x, y, vector) {
-        return new Monster(x, y, vector, 40, 25, 0.1, 15, 4);
+        return new Monster(x, y, vector, 40, black, 25, 0.1, 15, 4);
     }
 
     static speedy(x, y, vector) {
-        return new Monster(x, y, vector, 30, 4, 0.5, 10, 2);
+        return new Monster(x, y, vector, 30, blue, 4, 0.5, 10, 2);
     }
 
     static assassin(x, y, vector) {
-        return new Monster(x, y, vector, 30, 5, 0.15, 60, 5);
+        return new Monster(x, y, vector, 30, red, 5, 0.15, 60, 5);
     }
 
     draw() {
-        fill(black);
+        fill(this.debugColor);
         noStroke();
         circle(this.x, this.y, this.size);
     }
